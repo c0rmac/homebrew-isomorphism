9b33f597bee0e909a20773fda9ac6d860af331bf
Cormac Cinnsealach <cormacpjk00@gmail.com>
2026-03-13 17:31:27 +0100

    v0.2.0

--- Formula/isomorphism.rb
+++ Formula/isomorphism.rb
@@ -1,8 +1,8 @@
 class Isomorphism < Formula
   desc "Platform-agnostic C++ math wrapper unifying MLX and SYCL backends"
   homepage "https://github.com/c0rmac/isomorphism"
-  url "https://github.com/c0rmac/isomorphism/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4549f3c16ac048d9dbe13cd2aa4c38bcc899ce400e14ce7c4ea53ecb2e64342a" # Generate using 'shasum -a 256' on the tarball
+  url "https://github.com/c0rmac/isomorphism/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "71dab3bf971ea5a7c78323aa48e1436653de9038643ea1ca9093de8a4d983c6f" # Generate using 'shasum -a 256' on the tarball
   license "MIT"
 
   depends_on "cmake" => :build
